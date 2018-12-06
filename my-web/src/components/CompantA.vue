23b5740ed0b64aeb3aff423aa1ca73fbd7b5a8c8
fengjf <12345123>
2018-12-06 14:16:49 +0800

    前端页面

---
+++ my-web/src/components/CompantA.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="CompantA">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Compant from A!!!!'
+      }
+    },
+
+    props: [],
+
+    methods() {
+
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
