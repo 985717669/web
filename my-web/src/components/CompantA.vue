d14601ea65eb0c275aea6121e465a5a3af28f0f1
fengjf <12345123>
2018-12-06 14:58:45 +0800

    第一个开始

--- my-web/src/components/CompantA.vue
+++ my-web/src/components/CompantA.vue
@@ -1,26 +1,39 @@
 <template>
-  <div class="CompantA">
+  <div class="vue_det">
     <h1>{{ msg }}</h1>
+
+    <div v-html="message"/>
+
+    <label for="r1">修改颜色</label><input type="checkbox" v-model="class1" id="r1">
+
+    <br><br>
+    <div v-bind:class="{'class1': class1}">
+      <p>v-bind:class 指令</p>
+    </div>
+
   </div>
 </template>
 
 <script>
   export default {
-    data() {
+    data () {
       return {
-        msg: 'Compant from A!!!!'
+        msg: '非常 非常欢迎您!!',
+        message: '<h1>菜鸟教程111</h1>',
+        class1: true
       }
     },
-
-    props: [],
-
-    methods() {
-
+    methods: {
+      details: function() {
+        return  this.site + " - 学的不仅是技术，更是梦想！";
+      }
     }
   }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+  p {
+    color: #42b983;
+  }
 </style>
